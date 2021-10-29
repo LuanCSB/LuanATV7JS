8ecf59ff8ec144e1e1b6096376bb712dd0f2b702
LuanCSB <90980245+LuanCSB@users.noreply.github.com>
2021-10-29 12:09:55 -0300

    Add files via upload

---
+++ Script.js
@@ -0,0 +1,10 @@
+const nome= prompt('digite seu nome')
+const nascimento= prompt('em que ano voçê nasceu?')
+const ano=2021
+let idade= ano-nascimento
+
+let mensagem= `${nome}, seja bem vindo a minha página! Sua idade é ${idade} anos`
+
+
+alert(mensagem)
+
